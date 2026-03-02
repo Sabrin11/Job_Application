1872b2c59250423755be6dc4aedf44f00b5b36c8
Sabrin11 <sabrinsadia000@gmail.com>
2026-03-02 09:03:08 +0600

    Final commit after complete all section and readmeFile

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function switchTab(tab) {
     console.log(tab);
     
     const tabs = ['all', 'interview', 'rejected'];
-    
+     current_tab = tab;
     for (const t of tabs) {
         const tab_name = document.getElementById("Tab-" + t);
         
@@ -39,13 +39,13 @@ function switchTab(tab) {
         if(interviewContainer.children.length<1){
             emptyStat.classList.remove("hidden");
         }
-    }else{
+   } else if (tab === 'rejected'){
         rejectContainer.classList.remove('hidden');
         if(rejectContainer.children.length<1){
             emptyStat.classList.remove("hidden");
         }
     }
-    current_tab = tab;
+    
 }
 
 // State
@@ -53,6 +53,7 @@ const totalStat=document.getElementById('state-total')
 const interviewStat=document.getElementById('state-interview')
 const rejectStat=document.getElementById('state-reject')
 const emptyStat=document.getElementById('empty');
+const availableStat=document.getElementById("available");
 
 switchTab(current_tab);
 
@@ -65,31 +66,33 @@ document.getElementById("jobs-container").addEventListener('click',function(even
     if(clickedElement.classList.contains('interview')){
         status.innerText='Interviewed';
         interviewContainer.append(card);
-        updateStat();
     }
     if(clickedElement.classList.contains('rejected')){
         status.innerText='Rejected';
         rejectContainer.append(card);
-        updateStat();
     }
     if(clickedElement.classList.contains('delete')){
         parent.removeChild(card);
-        updateStat();
     }
+    updateStat();
 })
 
 // Update status
 function updateStat(){
-    // totalStat.innerText=allContainer.children.length;
-    // interviewStat.innerText=interviewContainer.children.length;
-    // rejectStat.innerText=rejectContainer.children.length;
     const counts={
         all :allContainer.children.length,
         interview :interviewContainer.children.length,
         rejected :rejectContainer.children.length
     };
-    totalStat.innerText=counts['all']
-    interviewStat.innerText=counts['interview']
-    rejectStat.innerText=counts['rejected']
+    totalStat.innerText=counts.all;
+    interviewStat.innerText=counts['interview'] ;
+    rejectStat.innerText=counts['rejected'];
+    availableStat.innerText=counts[current_tab]
+
+    if(counts[current_tab]<1){
+        emptyStat.classList.remove("hidden");
+    }else{
+        emptyStat.classList.add("hidden");
+    }
 }
 updateStat();
